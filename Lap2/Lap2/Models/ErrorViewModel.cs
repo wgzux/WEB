596d636df97dce5e6a2e174a806097cfd94d4122
wgzux <pqvu.92nvt@gmail.com>
2025-12-01 17:13:14 +0700

    init project LAB2

---
+++ Lap2/Lap2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lap2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
